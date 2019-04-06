9cd076c67bceaa7d31d31e0bd06aa92a030a59a3
brandonekbatani <47122420+brandonekbatani@users.noreply.github.com>
2019-04-06 10:59:41 -0700

    Added filter.php, and button extensions
    Created Filter.php page (in progress), updated nav bar buttons to direct to filter.php

--- filter.php
+++ filter.php
@@ -40,42 +40,6 @@ if (isset($_SESSION['username'])) {
 			<a href="out.php"><i class="fas fa-sign-out-alt"></i></a>
 		</nav>
 	</div>
-	<div class="little_profile">
-		<div class="user column">
-			<a href="<?php echo $user_signin;?>">
-				<img src="<?php echo $user['profile_pic'];?>">
-			</a>
-			<div class="index_user">
-				<a href="<?php echo $user_signin;?>">
-					<?php
-						echo $user['firstname'] . " " . $user['lastname'] . "<br>";
-					?>
-				</a>
-				<div class=profile_info>
-					<?php echo "Driver Rating : " . $user['rating_driver'] . "<br>";?>
-					<?php echo "Rider Rating : " . $user['rating_rider'];?>
-				</div>
-			</div>
-		</div>
-	</div>
-	<div class="newsfeed column">
-		<form class="create_post" action="addComment.php" method="POST">
-			<textarea name="new_text" id="new_text" placeholder="What's on your mind, <?php echo $user['firstname'];?>?"></textarea>
-			<input type="submit" name="new_post" id="post_button" value="Post">
-		</form>
-	</div>
-	<table>
-		<?php
-			$sql = "SELECT content FROM comments WHERE owner = '$user_signin '";
-			$query = mysqli_query($con, $sql);
-			while ($row = mysqli_fetch_row($query)) {
-				echo "<tr>";
-				echo ('<div class="newsfeed column">');
-			  echo stripslashes(utf8_decode($row[0]));
-			}
-			echo "</div>";
-			echo "</tr>";
-		?>
-	</table>
+
 </body>
 </html>
