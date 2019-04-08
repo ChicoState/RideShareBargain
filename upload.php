89bca5be154b5e244b0ec20c7931b5c35176852c
asahnoun <35755175+asahnoun@users.noreply.github.com>
2019-04-08 14:12:17 -0700

    Change name of pictures uploaded to random names

--- upload.php
+++ upload.php
@@ -11,11 +11,11 @@ if (isset($_SESSION['username'])) {
 ?>
 <?php
 $target_dir = "styling/img/profile_pic/";
-$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
 $uploadOk = 1;
-$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
-// Check if image file is a actual image or fake image
 if(isset($_POST["submit"])) {
+    $imageFileType = end(explode('.', $_FILES["fileToUpload"]["name"]));
+    $name = md5(rand()) . '.' . $imageFileType;
+    $target_file = $target_dir . $name;
     $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
     if($check !== false) {
         echo "File is an image - " . $check["mime"] . ".";
