d667e7fd072e1383ad096d0e793daffd27fbc32e
brandonekbatani <47122420+brandonekbatani@users.noreply.github.com>
2019-04-08 09:42:39 -0700

    Filter Page Beta Complete
    Filter page to search for Posts Via Date

--- filter.php
+++ filter.php
@@ -37,9 +37,117 @@ if (isset($_SESSION['username'])) {
 			<a href="#"><i class="fas fa-envelope"></i></a>
 			<a href="filter.php"><i class="fas fa-car-alt"></i></a>
 			<a href="#"><i id="settings" class="fas fa-cog"></i></a>
-			<a href="out.php"><i class="fas fa-sign-out-alt"></i></a>
+			<a href="signout.php"><i class="fas fa-sign-out-alt"></i></a>
 		</nav>
 	</div>
+	<?php
+	$conn = mysqli_connect("localhost", "root", "", "rsb");
+	
+	$post_at = "";
+	$post_at_to_date = "";
+	
+	$queryCondition = "";
+	if(!empty($_POST["search"]["post_at"])) {			
+		$post_at = $_POST["search"]["post_at"];
+		list($fid,$fim,$fiy) = explode("-",$post_at);
+		
+		$post_at_todate = date('Y-m-d');
+		if(!empty($_POST["search"]["post_at_to_date"])) {
+			$post_at_to_date = $_POST["search"]["post_at_to_date"];
+			list($tid,$tim,$tiy) = explode("-",$_POST["search"]["post_at_to_date"]);
+			$post_at_todate = "$tiy-$tim-$tid";
+		}
+		
+		$queryCondition .= "WHERE date BETWEEN '$fiy-$fim-$fid' AND '" . $post_at_todate . "'";
+	}
+	if($_POST)
+	{
+	$sql = "SELECT * from comments " . $queryCondition . " ORDER BY date desc";
+	$result = mysqli_query($conn,$sql);
+	}
+?>
+
+<html>
+	<head>
+    <title>Search for a Ride!</title>		
+	<script src="http://code.jquery.com/jquery-1.9.1.js"></script>
+	<link rel="stylesheet" href="http://code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css">
+
+	<style>
+	.table-content{border-top:#CCCCCC 4px solid; width:100%;}
+	.table-content th {padding:5px 20px; background: #F0F0F0;vertical-align:top;} 
+	.table-content td {padding:10px 25px; border-bottom: #F0F0F0 1px solid;vertical-align:top;} 
+	input[type=text], select {
+  width:25%;
+  padding: 10px 30px;
+  margin: 8px 0;
+  
+
+  border-radius: 10px;
+  box-sizing: border-box;
+  
+}
+
+
+	</style>
+	</head>
+	
+	<body>
+    <div class="filter-ride">
+		<h2 class="Searh4Ride">Search for a Ride!</h2>
+  <form name="frmSearch" method="post" action="">
+	 <p class="search_input">
+		<input type="text" placeholder="From Date" id="post_at" name="search[post_at]"  value="<?php echo $post_at; ?>" class="input-control" />
+	    <input type="text" placeholder="To Date" id="post_at_to_date" name="search[post_at_to_date]" style="margin-left:10px"  value="<?php echo $post_at_to_date; ?>" class="input-control"  />			 
+		<input type="submit" name="go" value="Search" >
+	</p>
+<?php if(!empty($result))	 { ?>
+<table class="table-content">
+          <thead>
+        <tr>
+                      
+          <th width="30%"><span>Owner</span></th>
+          <th width="50%"><span>Description</span></th>          
+          <th width="20%"><span>Date Ride Posted</span></th>	  
+        </tr>
+      </thead>
+    <tbody>
+	<?php
+		while($row = mysqli_fetch_array($result)) {
+	?>
+        <tr>
+			<td><?php echo $row["owner"]; ?></td>
+			<td><?php echo $row["content"]; ?></td>
+			<td><?php echo $row["date"]; ?></td>
+
+		</tr>
+   <?php
+		}
+   ?>
+   <tbody>
+  </table>
+<?php } ?>
+  </form>
+  </div>
+<script src="http://code.jquery.com/ui/1.10.3/jquery-ui.js"></script>
+<script>
+$.datepicker.setDefaults({
+showOn: "button",
+buttonImage: "datepicker.png",
+buttonText: "Date Picker",
+buttonImageOnly: true,
+dateFormat: 'dd-mm-yy'  
+});
+$(function() {
+$("#post_at").datepicker();
+$("#post_at_to_date").datepicker();
+});
+</script>
+	
+	<div>
+
+
+</div>
 
 </body>
 </html>
