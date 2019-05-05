00041611a86a038d22151e63cadf33950d6398f8
asahnoun <35755175+asahnoun@users.noreply.github.com>
2019-05-05 13:48:45 -0700

    Add dynamically Post inputs

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ include ("Post.php");
 include ('chat.php');
 if(isset($_POST['post'])) {
 	$post = new Post($con, $user_signin);
-	$post->submit_post($_POST['new_text'], 'none');
+	$post->submit_post($_POST['new_text'],$_POST['from'],$_POST['destination'],$_POST['price'],$_POST['date'],$_POST['usr_time'], 'none');
 }
 ?>
 	<div class="little_profile">
@@ -28,8 +28,59 @@ if(isset($_POST['post'])) {
 	</div>
 	<div class="newsfeed column">
 		<form class="create_post" action="index.php" method="POST">
-			<textarea name="new_text" id="new_text" placeholder="What's on your mind, <?php echo $user['firstname'];?>?"></textarea>
-			<input type="submit" name="post" id="post_button" value="Post">
+			<table>
+				<tr>
+					<div class="fromTo" id="fromTo">
+						<td>
+								<div class="from" id="from">
+									<h4>From : </h4>
+									<select name="from" required>
+										<option value="Chico">Chico</option>
+										<option value="Sacramento">Sacramento</option>
+										<option value="SF">San Francisco</option>
+										<option value="LA">Los Angeles</option>
+									</select>
+							</div>
+						</td>
+						<td>
+							<div class="destination" id="destination">
+								<h4>Destination : </h4>
+								<select name="destination" required>
+									<option value="Chico">Chico</option>
+									<option value="Sacramento" selected="selected">Sacramento</option>
+									<option value="SF">San Francisco</option>
+									<option value="LA">Los Angeles</option>
+								</select>
+							</div>
+						</td>
+					</div>
+				</tr>
+				<tr>
+					<td>
+						<br/>
+						<h4>
+							Date:
+						</h4>
+						 <input type="date" name="date" required>
+						<script>
+						var today = new Date().toISOString().split('T')[0];
+						document.getElementsByName("date")[0].setAttribute('min', today);
+						</script>
+					</td>
+					<td>
+						<br/>
+						<h4>
+							Time :
+						</h4>
+						<input type="time" name="usr_time"  required>
+						
+					</td>
+				</tr>
+				<br/>
+				<tr><td><h4>	Price :</h4><input type="number" name="price" min="0" required></td></tr>
+			</table>
+				<textarea name="new_text" id="new_text" placeholder="Any request or preference <?php echo $user['firstname'];?>?" required></textarea>
+				<input type="submit" name="post" id="post_button" value="Post">
 		</form>
 		<?php
 			$post = new Post($con, $user_signin);
