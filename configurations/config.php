4c23aaea7c765fae1e1256cbd63c2663dfea72c8
asahnoun <35755175+asahnoun@users.noreply.github.com>
2019-05-12 13:22:17 -0700

    fix configuration by deleting password

--- configurations/config.php
+++ configurations/config.php
@@ -2,7 +2,7 @@
 ob_start(); // turns on output buffering
 session_start();
 $timezone = date_default_timezone_set("America/Los_Angeles");
-$con = mysqli_connect("localhost", "root", "smit", "rsb");
+$con = mysqli_connect("localhost", "root", "", "rsb");
 if (mysqli_connect_errno()) {
 	echo "Failed to connect: " . mysqli_connect_errno();
 }
